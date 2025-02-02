2875c13c78abf700fc92ae897036a88c9d69adc5
Andreea Iaconi <iaconiandreea@gmail.com>
2025-02-02 08:12:02 +0000

    feat(nav): add responsive navbar with animated hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./style.css";
+
+
+const hamburger = document.querySelector(".nav__hamburger");
+const linksContainer  = document.querySelector(".nav__menu");
+const links = document.querySelectorAll(".nav__menu__link");  
+
+hamburger.addEventListener("click", () => {
+    linksContainer.classList.toggle("active");
+    hamburger.classList.toggle("active");
+
+})
+
+window.addEventListener("resize", () => {
+    if (window.matchMedia("(max-width: 900px)").matches) {
+        closeMenu();
+    }
+})
+
+if (window.matchMedia("(max-width: 900px)").matches) {
+    closeMenu();
+}
+
+function closeMenu() {
+    links.forEach((link) =>{
+        link.addEventListener("click", () => {
+             linksContainer.classList.remove("active");
+             hamburger.classList.remove("active");
+             
+        })
+    })
+}
